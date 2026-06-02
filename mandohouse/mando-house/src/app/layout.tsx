7369781f86407f6e8c0ee96aa385bbf9c178665c
ingchuon <ingchuon12@gmail.com>
2026-06-02 16:07:53 +0700

    Update layout.tsx

--- mandohouse/mando-house/src/app/layout.tsx
+++ mandohouse/mando-house/src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="th">
       <head>
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Mando House" />
