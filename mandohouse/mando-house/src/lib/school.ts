9f99d8e596130bec0da9b5a7ab16d3ef6341d955
ingchuon <ingchuon12@gmail.com>
2026-07-22 22:07:05 +0700

    Delete mandohouse/mando-house/src/lib/school.ts

--- mandohouse/mando-house/src/lib/school.ts
+++
@@ -1,7 +0,0 @@
-// src/lib/school.ts
-import { cookies } from 'next/headers'
-
-export function getSchoolId(): string {
-  const cookieStore = cookies()
-  return cookieStore.get('school_id')?.value ?? 'mando'
-}
